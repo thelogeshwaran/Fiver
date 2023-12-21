ec36eb71a2106b0058f57bbaebc597f59fc09437
logeshwarand <47322586+thelogeshwaran@users.noreply.github.com>
2023-12-21 23:50:33 +0530

    home screen services section

--- client/src/components/FiverrBussines.jsx
+++ client/src/components/FiverrBussines.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import { BsFillPatchCheckFill } from "react-icons/bs";
+import FiverLogo from "./FiverLogo";
 
 function FiverrBussines() {
-    return (
-        <div>FiverBussines</div>
-    )
+  return (
+    <div className="flex px-20 py-16 bg-[#0d074d] text-white">
+      <div>
+        <div className="flex gap-2">
+          <FiverLogo fillColor={"#ffff"} />
+          <div className="text-xl">Business Solutions</div>
+        </div>
+        <div className="text-3xl font-bold my-3 w-2/4">
+          Advanced solutions and professional talent for business
+        </div>
+        <ul className="flex flex-col gap-5 w-3/4 mt-8">
+          <li className="flex items-center gap-3">
+            <BsFillPatchCheckFill color="#b2abff" size={25} />
+            <div>
+              <div className="font-bold ">Firver Pro</div>
+              <div>
+                Access top freelancers and professional business tools for any
+                project
+              </div>
+            </div>
+          </li>
+          <li className="flex items-center gap-3">
+            <BsFillPatchCheckFill color="#b2abff" size={25} />
+            <div>
+              <div className="font-bold ">Fiverr Certified</div>
+              <div>Build your own branded marketplace of certified experts</div>
+            </div>
+          </li>
+          <li className="flex items-center gap-3">
+            <BsFillPatchCheckFill color="#b2abff" size={25} />
+            <div>
+              <div className="font-bold ">Fiverr Enterprise</div>
+              <div>
+                Manage your freelance workforce and onboard additional talent
+                with an end-to-end SaaS solution
+              </div>
+            </div>
+          </li>
+        </ul>
+        <button className="bg-[#ffffff] text-black p-2 px-3 my-3 rounded-md">
+          Learn more
+        </button>
+      </div>
+      <div className="relative h-512px w-2/3">
+        <Image src="/business.webp" fill />
+      </div>
+    </div>
+  );
 }
 
-export default FiverrBussines
+export default FiverrBussines;
--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { categories } from "@/utils/categories";
+import Image from "next/image";
+import React from "react";
 
 function Services() {
-    return (
-        <div>Services</div>
-    )
+  return (
+    <div className="flex mx-20 flex-col my-16 gap-5">
+      <div className="text-4xl">You need it, we've got it</div>
+      <div>
+        <ul className="grid grid-cols-5 gap-10">
+          {categories.map((item, index) => {
+            return (
+              <li className="flex flex-col justify-center items-center cursor-pointer hover:shadow-2xl border-white hover:border-[#1cbf73] border-2 p-5 transition-all duration-500">
+                <Image src={item.logo} height={50} width={50} />
+                <div>{item.name}</div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default Services
+export default Services;
